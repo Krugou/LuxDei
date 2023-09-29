bb4ee8fb3d69449bcfb2d32ad144357d8bf56fec
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-29 18:46:14 +0300

    alotetaas

--- frontend/src/components/header/HeaderTitle.jsx
+++ frontend/src/components/header/HeaderTitle.jsx
@@ -1,11 +1,11 @@
-
 const HeaderTitle = () => {
-    return (
-        <>
-            <h1 className='flex flex-col text-blue  '>Welcome to JakFilms Festival. Fils de chien
-            </h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className='flex flex-col text-blue'>
+        Welcome to JakFilms Festival. Fils de chien
+      </h1>
+    </>
+  );
 };
 
 export default HeaderTitle;
---
+++ frontend/src/components/main/Articles.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Articles = () => {
+  return <div>body</div>;
+};
+
+export default Articles;
