219399e74ec1cdf619bb1bf7addb8eff3245ede4
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-29 08:21:50 +0300

    Update Header.jsx

--- frontend/src/views/Header.jsx
+++ frontend/src/views/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavUpperElement from '../components/header/NavUpperElement';
 
 const Header = () => {
     return (
         <header>
+            <NavUpperElement />
             <h1 className='flex flex-col'>My Website</h1>
             <nav>
                 <ul>
