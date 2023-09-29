b6bf41c8330b134d62f8b2e2c8e218327431103c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-29 09:11:34 +0300

    Update HeaderTitle.jsx

--- frontend/src/components/header/HeaderTitle.jsx
+++ frontend/src/components/header/HeaderTitle.jsx
@@ -2,7 +2,7 @@
 const HeaderTitle = () => {
     return (
         <>
-            <h1 className='flex flex-col text-blue  '>Welcome to JakFilms Festival Fils de chien
+            <h1 className='flex flex-col text-blue  '>Welcome to JakFilms Festival. Fils de chien
             </h1>
         </>
     );
