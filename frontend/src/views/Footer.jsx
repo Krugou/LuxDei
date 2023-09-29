fe88c4553679851960d178ea5e301971a1556fbf
Krugou <59641229+Krugou@users.noreply.github.com>
2023-09-29 20:12:16 +0300

    Update Footer.jsx

--- frontend/src/views/Footer.jsx
+++ frontend/src/views/Footer.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <p>This is the footer.</p>
+        <footer className="bg-gray-800 text-gray-400 py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <p>&copy; 2021 My Website</p>
+                <ul className="flex space-x-4">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </footer>
     );
 };
