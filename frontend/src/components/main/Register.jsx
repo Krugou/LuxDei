5fa022962d03c3a5d5163dcf84ca1421dfed4407
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:48:07 +0300

    Update Register.jsx

--- frontend/src/components/main/Register.jsx
+++ frontend/src/components/main/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Register = () => {
     const [username, setUsername] = useState('');
@@ -12,20 +12,40 @@ const Register = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <label className="flex flex-col items-start mt-4">
+                <span className="text-lg font-medium mb-2">Username:</span>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    className="border border-gray-400 rounded-lg p-2 w-full"
+                />
             </label>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+            <label className="flex flex-col items-start mt-4">
+                <span className="text-lg font-medium mb-2">Email:</span>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    className="border border-gray-400 rounded-lg p-2 w-full"
+                />
             </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+            <label className="flex flex-col items-start mt-4">
+                <span className="text-lg font-medium mb-2">Password:</span>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    className="border border-gray-400 rounded-lg p-2 w-full"
+                />
             </label>
-            <button type="submit">Register</button>
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            >
+                Register
+            </button>
         </form>
     );
 };
