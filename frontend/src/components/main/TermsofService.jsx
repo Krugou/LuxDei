89099c32e3e0ef8c0fa00b2f343d6a27fc01b34f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:53:18 +0300

    Create TermsofService.jsx

---
+++ frontend/src/components/main/TermsofService.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const TermsOfService = () => {
+    return (
+        <div className="bg-gray-100  py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto">
+                <h1 className="text-3xl font-extrabold text-gray-900">Terms of Service</h1>
+                <p className="mt-2 text-gray-600">
+                    Welcome to JakFilms Movie Festival. By using our website, you agree to comply with and be bound by the following terms and conditions.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">1. Acceptance of Terms</h2>
+                <p className="mt-2 text-gray-600">
+                    By accessing or using our website, you acknowledge that you have read, understood, and agree to be bound by these Terms of Service.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">2. Use of the Website</h2>
+                <p className="mt-2 text-gray-600">
+                    You may use our website for your personal and non-commercial use. You agree not to use the website for any unlawful purpose.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">3. Privacy Policy</h2>
+                <p className="mt-2 text-gray-600">
+                    Your use of our website is also governed by our Privacy Policy. Please review our Privacy Policy for more information.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">4. Changes to Terms</h2>
+                <p className="mt-2 text-gray-600">
+                    We reserve the right to modify or replace these Terms of Service at any time without prior notice. Please review these terms regularly.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">5. Contact Information</h2>
+                <p className="mt-2 text-gray-600">
+                    If you have any questions about these Terms of Service, please contact us at info@jakfilms.com.
+                </p>
+
+                {/* Add more sections as needed */}
+            </div>
+        </div>
+    );
+};
+
+export default TermsOfService;
