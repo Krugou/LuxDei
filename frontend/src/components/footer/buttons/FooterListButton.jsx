5d0f04917e34d8c44c96ffd9b49786d914130e2a
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:35:04 +0300

    Create FooterListButton.jsx

---
+++ frontend/src/components/footer/buttons/FooterListButton.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import {useNavigate} from 'react-router-dom';
+
+const FooterListButton = ({name}) => {
+    const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+    name = name.replace(/\s+/g, '');
+    const navigate = useNavigate();
+    return (
+        <li className='p-2'>
+            <button
+                onClick={() => {
+                    navigate(`/${name}`);
+                }}
+                className='border rounded m-2  p-2 '
+            >
+                {capitalized}
+            </button>
+        </li>
+    );
+};
+
+FooterListButton.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+
+export default FooterListButton;
