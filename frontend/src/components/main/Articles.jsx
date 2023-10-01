63d92e7610a47d129af1f963c3da21ddfa153f72
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:41:19 +0300

    Update Articles.jsx

--- frontend/src/components/main/Articles.jsx
+++ frontend/src/components/main/Articles.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
 const Articles = () => {
-  return <div>body</div>;
+  const articles = [
+    {
+      id: 1,
+      title: 'Article 1',
+      content: 'This is the content of article 1.'
+    },
+    {
+      id: 2,
+      title: 'Article 2',
+      content: 'This is the content of article 2.'
+    },
+    {
+      id: 3,
+      title: 'Article 3',
+      content: 'This is the content of article 3.'
+    }
+  ];
+
+  return (
+    <div>
+      <h1>Articles</h1>
+      {articles.map(article => (
+        <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Articles;
