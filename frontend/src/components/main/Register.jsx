8439310e9c807c030e7c6b4a0cdc7306cade92fb
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 21:57:33 +0300

    Update Register.jsx

--- frontend/src/components/main/Register.jsx
+++ frontend/src/components/main/Register.jsx
@@ -4,11 +4,35 @@ const Register = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [country, setCountry] = useState('fi');
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
         // Add code here to submit form data to server
+        fetch('http://jakfilms.northeurope.cloudapp.azure.com/backend/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: username,
+                email: email,
+                password: password,
+                countryid: country,
+            }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     };
 
     return (
@@ -39,6 +63,19 @@ const Register = () => {
                     onChange={(event) => setPassword(event.target.value)}
                     className="border border-gray-400 rounded-lg p-2 w-full"
                 />
+            </label>
+            <label className="flex flex-col items-start mt-4">
+                <span className="text-lg font-medium mb-2">Country:</span>
+                <select
+                    value={country}
+                    onChange={(event) => setCountry(event.target.value)}
+                    className="border border-gray-400 rounded-lg p-2 w-full"
+                >
+                    <option value="fi">Finland</option>
+                    <option value="dk">Denmark</option>
+                    <option value="no">Norway</option>
+                    <option value="se">Sweden</option>
+                </select>
             </label>
             <button
                 type="submit"
