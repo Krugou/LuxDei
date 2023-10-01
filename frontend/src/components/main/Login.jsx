b8621d520663aa4e71f2dd276081798780000f87
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:48:11 +0300

    Update Login.jsx

--- frontend/src/components/main/Login.jsx
+++ frontend/src/components/main/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -11,20 +11,47 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center ">
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter your username"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Sign In
+                    </button>
+
+                </div>
+            </div>
         </form>
     );
 };
+
 
 export default Login;
