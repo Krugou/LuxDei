8e7fb90a764c975e1d7755e1a70dd0bf133d3c63
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:41:09 +0300

    Create Login.jsx

---
+++ frontend/src/components/main/Login.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Username: ${username}, Password: ${password}`);
+        // Add logic to authenticate user here
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Login;
