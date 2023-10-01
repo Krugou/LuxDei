b07bfc9381c1b11e6d62e6a30fe279be99d02b6b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:53:14 +0300

    Create PrivacyPolicy.jsx

---
+++ frontend/src/components/main/PrivacyPolicy.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className="bg-gray-100  py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto">
+                <h1 className="text-3xl font-extrabold text-gray-900">Privacy Policy</h1>
+                <p className="mt-2 text-gray-600">
+                    Your privacy is important to us. This Privacy Policy explains how we collect, use, disclose, and safeguard your personal information.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">Information We Collect</h2>
+                <p className="mt-2 text-gray-600">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ex in quam feugiat sollicitudin.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">How We Use Your Information</h2>
+                <p className="mt-2 text-gray-600">
+                    Vestibulum tristique nisi eget dolor tincidunt, ut facilisis odio vestibulum.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">Disclosure of Your Information</h2>
+                <p className="mt-2 text-gray-600">
+                    Proin sed tortor in dolor varius bibendum in nec purus. Fusce vel lectus lectus.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">Security</h2>
+                <p className="mt-2 text-gray-600">
+                    Ut pharetra erat nec ligula feugiat, non lacinia purus dictum. Donec vel cursus urna.
+                </p>
+
+                <h2 className="mt-4 text-xl font-semibold text-gray-800">Changes to This Privacy Policy</h2>
+                <p className="mt-2 text-gray-600">
+                    Aenean vitae sapien nec dui iaculis ullamcorper. Sed congue lectus at laoreet euismod.
+                </p>
+
+                {/* Add more sections as needed */}
+            </div>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
