0d60e0c22206f4f1d6bd03dac4e1dc9946c77d32
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-01 09:34:58 +0300

    Create FooterList.jsx

---
+++ frontend/src/components/footer/FooterList.jsx
@@ -0,0 +1,28 @@
+import FooterListButton from "./buttons/FooterListButton";
+
+const FooterList = () => {
+
+    return (
+        <div className="container mx-auto flex justify-between items-center">
+            <p>&copy; {new Date().getFullYear()} JakFilms</p>
+            <ul className="flex flex-col ">
+                <FooterListButton name="home" />
+                <FooterListButton name="about" />
+            </ul>
+
+            <ul className="flex flex-col">
+                <FooterListButton name="login" />
+                <FooterListButton name="register" />
+
+            </ul>
+            <ul className="flex flex-col ">
+                <FooterListButton name="terms of service" />
+                <FooterListButton name="privacy policy" />
+
+            </ul>
+        </div>
+    );
+
+};
+
+export default FooterList;
