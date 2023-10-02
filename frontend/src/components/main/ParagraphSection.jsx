dddd7a11ee5b8eb23fb41cbbec6a104e43d1d4b9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-02 15:09:14 +0300

    Create ParagraphSection.jsx

---
+++ frontend/src/components/main/ParagraphSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ParagraphSection = ({content}) => {
+    return (
+        <p className="text-lg my-2">{content}</p>
+    );
+};
+
+export default ParagraphSection;
