120e2253c92a497318ceca1eb286b9d3efc52de9
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-02 22:41:09 +0300

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -7,30 +7,32 @@ import httpError from '../utils/errors.js';
 const router = express.Router();
 
 // Define a separate function for handling passport authentication
-const authenticate = (req, res) => {
+const authenticate = (req, res, next) => {
   console.log('authenticate maan');
   passport.authenticate('local', { session: false }, (err, user, info) => {
     console.log('info: ', info);
     console.log('err1: ', err);
     if (err || !user) {
-      return httpError('Virhe kirjautuessa', 403);
+      next(httpError('Virhe kirjautuessa', 403));
+      return;
     }
     req.login(user, { session: false }, (err) => {
       if (err) {
         console.log('err2: ', err);
-        return next(httpError('Virhe kirjautuessa', 403));
+        next(httpError('Virhe kirjautuessa', 403));
+        return;
       }
       const token = jwt.sign(user, process.env.JWT_SECRET);
       res.json({ user, token });
     });
-  })(req, res);
+  })(req, res, next);
 };
 
 // Define validation and error handling for the login route
 router.post(
   '/login',
   [body('username').escape(), body('password').escape()],
-  (req, res) => {
+  (req, res, next) => {
     // Extract the validation errors from a request.
     const errors = validationResult(req);
 
@@ -45,7 +47,7 @@ router.post(
     }
 
     // Call the authenticate function to handle passport authentication
-    authenticate(req, res);
+    authenticate(req, res, next);
   }
 );
 
