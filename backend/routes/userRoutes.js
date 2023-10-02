0bf65f2f41cc566b61fa7eb7ee13ac9cd64be2ff
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-02 22:06:37 +0300

    yeah man

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,9 @@
 
 import express from 'express';
 import User from '../models/User.js';
-//const { httpError } = require('../utils/errors');
-
-//const { validationResult } = require('express-validator');
-//const bcrypt = require('bcryptjs');
+import httpError from '../utils/errors.js';
+import { validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -46,7 +45,7 @@ router.get('/:id', (req, res) => {
 router.post('/', async (req, res) => {
   console.log('router post');
   // Extract the validation errors from a request.
-  /*
+
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -56,11 +55,11 @@ router.post('/', async (req, res) => {
 
     return httpError('Invalid data', 400);
   }
-  */
+
   try {
     // encrypt password
-    // const salt = bcrypt.genSaltSync(10);
-    //const pwd = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    const pwd = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
