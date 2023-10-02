39c7d57a6e18a3faa90cc28505344dabc170106c
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-02 15:52:35 +0300

    build

--- backend/jakfilmsserver.js
+++ backend/jakfilmsserver.js
@@ -2,8 +2,8 @@
 /* simple chat app */
 
 import express from 'express';
-import {createServer} from 'http';
-import {Server} from 'socket.io';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const http = createServer(app);
@@ -17,56 +17,70 @@ const maxSavedMessages = 20;
 app.use(express.static('jakfilms'));
 
 io.on('connection', (socket) => {
+  console.log(socket.id, ' has entered the building');
+  const ip = socket.request.connection.remoteAddress;
+  console.log(`Client connected with IP address: ${ip}`);
 
-    console.log(socket.id, ' has entered the building');
-    const ip = socket.request.connection.remoteAddress;
-    console.log(`Client connected with IP address: ${ip}`);
-
-    socket.on('disconnect', () => {
-        console.log(socket.id, ' has left the building');
-    });
-    socket.on('join room', (room) => {
-        // console.log(socket.id, ' joined room: ', room);
-        socket.join(room);
-        if (lastMessages) {
-            for (const room in lastMessages) {
-                if (lastMessages.hasOwnProperty(room)) {
-                    lastMessages[room].forEach((message) => {
-                        socket.emit('chat message', {message: message.message, username: message.username, room: message.room});
-                    });
-                }
-            }
-        }
-    });
-    socket.on('leave room', (room) => {
-        console.log(socket.id, ' left room: ', room);
-        socket.leave(room);
-    });
-    socket.on('chat message', (data) => {
-        console.log('chat message received:', data);
-        if (!lastMessages[data.room]) {
-            lastMessages[data.room] = [];
-        }
-        if (lastMessages[data.room].length > maxSavedMessages) {
-            lastMessages[data.room].shift();
-        }
-        lastMessages[data.room].push(data);
-        console.log(`message: ${data.message}, username: ${data.username}, room: ${data.room}`);
-        io.to(data.room).emit('chat message', {message: data.message, username: data.username, room: data.room});
-    });
-    socket.on('get messages', (room) => {
-        console.log('get messages for room: ', room);
-        if (lastMessages[room]) {
-            lastMessages[room].forEach((message) => {
-                console.log('message: ', message);
-                socket.emit('chat message', {username: message.username, message: message.message});
+  socket.on('disconnect', () => {
+    console.log(socket.id, ' has left the building');
+  });
+  socket.on('join room', (room) => {
+    // console.log(socket.id, ' joined room: ', room);
+    socket.join(room);
+    if (lastMessages) {
+      for (const room in lastMessages) {
+        if (lastMessages.hasOwnProperty(room)) {
+          lastMessages[room].forEach((message) => {
+            socket.emit('chat message', {
+              message: message.message,
+              username: message.username,
+              room: message.room,
             });
+          });
         }
+      }
+    }
+  });
+  socket.on('leave room', (room) => {
+    console.log(socket.id, ' left room: ', room);
+    socket.leave(room);
+  });
+  socket.on('chat message', (data) => {
+    console.log('chat message received:', data);
+    if (!lastMessages[data.room]) {
+      lastMessages[data.room] = [];
+    }
+    if (lastMessages[data.room].length > maxSavedMessages) {
+      lastMessages[data.room].shift();
+    }
+    lastMessages[data.room].push(data);
+    console.log(
+      `message: ${data.message}, username: ${data.username}, room: ${data.room}`
+    );
+    io.to(data.room).emit('chat message', {
+      message: data.message,
+      username: data.username,
+      room: data.room,
     });
+  });
+  socket.on('get messages', (room) => {
+    console.log('get messages for room: ', room);
+    if (lastMessages[room]) {
+      lastMessages[room].forEach((message) => {
+        console.log('message: ', message);
+        socket.emit('chat message', {
+          username: message.username,
+          message: message.message,
+        });
+      });
+    }
+  });
 });
 
-
-
 http.listen(port, () => {
-    console.log('Jakfilms backend server Started at: http://localhost:' + port + '/index.html ');
+  console.log(
+    'Jakfilms backend server Started at: http://localhost:' +
+      port +
+      '/index.html '
+  );
 });
