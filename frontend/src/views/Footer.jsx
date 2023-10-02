1cd62efd3d3bd8433c6d8a2f545c8e84dda83375
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-02 09:00:15 +0300

    Update Footer.jsx

--- frontend/src/views/Footer.jsx
+++ frontend/src/views/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import FooterList from '../components/footer/FooterList';
-
+import FooterCopyright from '../components/footer/FooterCopyright';
 const Footer = () => {
 
 
     return (
         <footer className="bg-gray-800 text-gray-400 py-4">
             <FooterList />
+            <FooterCopyright
+                companyName="JakFilms"
+            />
         </footer>
     );
 };
