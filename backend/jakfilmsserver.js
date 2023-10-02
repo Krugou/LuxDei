8499274ef889609a42abf42425932c0a4801cfca
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-02 18:07:44 +0300

    Update jakfilmsserver.js

--- backend/jakfilmsserver.js
+++ backend/jakfilmsserver.js
@@ -4,7 +4,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-
+import path from 'path';
 const app = express();
 const http = createServer(app);
 const io = new Server(http);
