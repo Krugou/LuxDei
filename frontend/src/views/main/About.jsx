db43a96cf86947452fc58c1710755595aab277a4
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-02 15:09:11 +0300

    Update About.jsx

--- frontend/src/views/main/About.jsx
+++ frontend/src/views/main/About.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
-
+import ParagraphSection from '../../components/main/ParagraphSection';
 const About = () => {
     return (
         <div className="bg-gray-100  p-8">
             <div className="container mx-auto">
                 <h1 className="text-3xl font-bold mb-4">About JakFilms Movie Festival</h1>
-                <p className="text-lg mb-6">
-                    Welcome to the JakFilms Movie Festival, where we celebrate the art of cinema!
-                    Immerse yourself in a world of storytelling, creativity, and entertainment.
-                    From classic films to cutting-edge cinema, we have it all.
-                </p>
-                <p className="text-lg">
-                    Join us for a week of movie screenings, discussions, and more.
-                    Don't miss our special events, including Q&A sessions with renowned directors,
-                    film workshops, and an exclusive awards ceremony to honor outstanding filmmakers.
-                </p>
-                <p className="text-lg mt-4">
-                    Whether you're a dedicated cinephile or simply looking for a great time,
-                    the JakFilms Movie Festival has something for everyone. Grab your popcorn,
-                    sit back, and enjoy the magic of the silver screen with us!
-                </p>
+                <ParagraphSection
+                    content="Welcome to the JakFilms Movie Festival, where we celebrate the art of cinema! Immerse yourself in a world of storytelling, creativity, and entertainment. From classic films to cutting-edge cinema, we have it all."
+                />
+                <ParagraphSection
+                    content="Join us for a week of movie screenings, discussions, and more. Don't miss our special events, including Q&A sessions with renowned directors, film workshops, and an exclusive awards ceremony to honor outstanding filmmakers."
+                />
+                <ParagraphSection
+                    content="Whether you're a dedicated cinephile or simply looking for a great time, the JakFilms Movie Festival has something for everyone. Grab your popcorn, sit back, and enjoy the magic of the silver screen with us!"
+                />
             </div>
         </div>
     );
