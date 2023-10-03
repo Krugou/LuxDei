feda3142c9fe48696b267af03f5f40b4bf81f920
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-04 00:10:31 +0300

    Update FooterCopyright.jsx

--- frontend/src/components/footer/FooterCopyright.jsx
+++ frontend/src/components/footer/FooterCopyright.jsx
@@ -5,7 +5,8 @@ const FooterCopyright = (props) => {
     const {companyName} = props;
 
     return (
-        <p className="text-center text-gray-500 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">&copy; {new Date().getFullYear()} {companyName}</p>
+        <p className="text-center text-gray-500 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl" aria-label={`Copyright ${new Date().getFullYear()} ${companyName}`}
+            title={`Copyright ${new Date().getFullYear()} ${companyName}`}>&copy; {new Date().getFullYear()} {companyName}</p>
     );
 };
 
