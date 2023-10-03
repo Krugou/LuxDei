5290138f21ff958dbb5214487b6b7759cb66722b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-04 00:10:21 +0300

    Update NotFound.jsx

--- frontend/src/views/main/NotFound.jsx
+++ frontend/src/views/main/NotFound.jsx
@@ -16,11 +16,13 @@ const NotFound = () => {
 
     return (
         <div className='bg-alepurple container mx-auto p-4 md:p-10 m-4'>
-            <h1 className="text-2xl md:text-4xl font-bold mybasetext">404 - Page Not Found</h1>
-            <p className="text-base md:text-lg mybasetext mt-2 md:mt-4">
+            <h1 className='text-2xl md:text-4xl font-bold mybasetext' aria-live='assertive'>
+                404 - Page Not Found
+            </h1>
+            <p className='text-base md:text-lg mybasetext mt-2 md:mt-4' aria-label='Error message'>
                 Oops! The page you are looking for does not exist.
             </p>
-            <p className="text-base md:text-lg mybasetext mt-2 md:mt-4">
+            <p className='text-base md:text-lg mybasetext mt-2 md:mt-4' aria-label='Instructions'>
                 It seems like you've taken a wrong turn. Please check the URL or navigate back to the home page.
             </p>
         </div>
