e7f5df1af554d1149f31c4930389b593a72d33f0
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-03 23:45:38 +0300

    Create NotFound.jsx

---
+++ frontend/src/views/main/NotFound.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const NotFound = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            navigate('/');
+        }, 10000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [navigate]);
+
+    return (
+        <div className='bg-alepurple container mx-auto p-4 md:p-10 m-4'>
+            <h1 className="text-2xl md:text-4xl font-bold mybasetext">404 - Page Not Found</h1>
+            <p className="text-base md:text-lg mybasetext mt-2 md:mt-4">
+                Oops! The page you are looking for does not exist.
+            </p>
+            <p className="text-base md:text-lg mybasetext mt-2 md:mt-4">
+                It seems like you've taken a wrong turn. Please check the URL or navigate back to the home page.
+            </p>
+        </div>
+    );
+};
+
+export default NotFound;
