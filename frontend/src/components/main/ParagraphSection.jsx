9615fdda72f26534932c1d82c9616d682a569617
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-04 00:10:27 +0300

    Update ParagraphSection.jsx

--- frontend/src/components/main/ParagraphSection.jsx
+++ frontend/src/components/main/ParagraphSection.jsx
@@ -1,9 +1,12 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 const ParagraphSection = ({content}) => {
     return (
-        <p className="text-lg my-2">{content}</p>
+        <p id='paragraph-section' className="text-lg my-2" aria-label='Paragraph section'>{content}</p>
     );
 };
-
+ParagraphSection.PropTypes = {
+    content: PropTypes.string.isRequired,
+};
 export default ParagraphSection;
