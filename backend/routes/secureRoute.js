f041b6befcc4c98ecc9e47462140c10a202de6d3
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-04 14:41:19 +0300

    Update secureRoute.js

--- backend/routes/secureRoute.js
+++ backend/routes/secureRoute.js
@@ -6,19 +6,13 @@ import passport from 'passport';
 import httpError from '../utils/errors.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  console.log(req.user, 'USER ASDASD');
   try {
-    if (req.user) {
-      // User info is available, send it as a JSON response
-      res.json({ user: req.user });
-    } else {
-      // User info is not available, send a 403 error response
-      res.status(403).json({ error: 'User info not available' });
-    }
+    res.json({ user: req.user });
   } catch (error) {
-    // Handle other errors (if any)
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    next(httpError('User info not available', 403));
+    return;
   }
 });
 export default router;
