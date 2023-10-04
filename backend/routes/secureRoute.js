1309d1e306ba11544da2b6a70c955f5746b4e831
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-04 14:11:19 +0300

    Update secureRoute.js

--- backend/routes/secureRoute.js
+++ backend/routes/secureRoute.js
@@ -7,6 +7,12 @@ import httpError from '../utils/errors.js';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  try {
+    return req.user;
+  } catch (error) {
+    next(httpError('User info not available', 403));
+    return;
+  }
   console.log(req.user, 'USER ASDASD');
 });
 export default router;
