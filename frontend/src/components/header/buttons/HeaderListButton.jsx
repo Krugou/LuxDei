be0450d58c8084ebe123bc4afa63a782ba9284b4
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-04 19:46:16 +0300

    Update HeaderListButton.jsx

--- frontend/src/components/header/buttons/HeaderListButton.jsx
+++ frontend/src/components/header/buttons/HeaderListButton.jsx
@@ -1,18 +1,33 @@
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PortraitIcon from '@mui/icons-material/Portrait';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 import PropTypes from 'prop-types';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import ArticleIcon from '@mui/icons-material/Article';
 import React from 'react';
-
 const HeaderListButton = ({name, navigate, lastItem = false}) => {
     const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
 
     return (
         <li>
             <button
-                className={`nav-button ${lastItem ? 'mx-auto' : 'mx-5'} text-lg text-white transition-all hover:text-gray-300 hover:text-xl`}
+                className={` nav-button ${lastItem ? 'mx-auto' : 'mx-5'} text-lg text-white transition-all hover:text-gray-300 hover:text-xl`}
                 onClick={() => {
                     navigate(`/${name}`);
                 }}
             >
-                {capitalized}
+                <div className="md:hidden">
+                    {name === 'login' && <LoginIcon />}
+                    {name === 'logout' && <LogoutIcon />}
+                    {name === 'profile' && <PortraitIcon />}
+                    {name === 'register' && <AppRegistrationIcon />}
+                    {name === 'articles' && <ArticleIcon />}
+                    {name === 'livestream' && <LiveTvIcon />}
+                    
+
+                </div>
+                <div className="hidden md:block">{capitalized}</div>
             </button>
         </li>
     );
