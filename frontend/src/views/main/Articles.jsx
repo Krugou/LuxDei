0205d2869c9040f766687984ad9027e41d08c9e5
KaarleH <kaarleh@metropolia.fi>
2023-10-04 14:19:06 +0300

    frontUpdate

--- frontend/src/views/main/Articles.jsx
+++ frontend/src/views/main/Articles.jsx
@@ -24,9 +24,9 @@ const Articles = () => {
 
 
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='bg-gray-100 p-8 mx-auto px-4 sm:px-6 lg:px-8'>
       <h1 className='text-3xl font-bold text-center mt-8 mb-4'>Articles</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {articles.map((article) => (
           <div key={article.id} className='bg-white rounded-lg shadow-lg mb-10 overflow-hidden'>
             <div className='p-4'>
