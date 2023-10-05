b3f9ef2ef7c0000e2ecc5ae9415f86a3fcfa2286
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-05 12:10:42 +0300

    Update Logout.jsx

--- frontend/src/views/main/Logout.jsx
+++ frontend/src/views/main/Logout.jsx
@@ -1,6 +1,6 @@
 import {useContext, useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import {UserContext} from '../contexts/UserContext';
+import {UserContext} from '../../contexts/UserContext';
 
 const Logout = () => {
   const {setUser} = useContext(UserContext);
