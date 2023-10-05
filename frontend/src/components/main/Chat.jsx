299e717074acad4852539f1abb9ba7580def5a67
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-05 12:58:35 +0300

    Update Chat.jsx

--- frontend/src/components/main/Chat.jsx
+++ frontend/src/components/main/Chat.jsx
@@ -167,7 +167,7 @@ const Chat = ({username, countryid}) => {
   }, [messages]);
   return (
     <>
-      {username && countryid ? (
+      
         <div className='flex flex-col  h-full   '>
           {typingUsers.length > 0 && (
             <div
@@ -299,28 +299,7 @@ const Chat = ({username, countryid}) => {
             </div>
           </form>
         </div>
-      ) : (
-        <div className='flex flex-col items-center justify-center '>
-          <h1 className='text-2xl m-2 font-bold mb-8'>
-            Please login or register to chat
-          </h1>
-          <div className='flex flex-col items-center'>
-            <button
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'
-              onClick={() => navigate('/login')}
-            >
-              Login
-            </button>
-            <button
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-              onClick={() => navigate('/register')}
-            >
-              Register
-            </button>
-          </div>
-        </div>
-
-      )}
+     
     </>
   );
 };
