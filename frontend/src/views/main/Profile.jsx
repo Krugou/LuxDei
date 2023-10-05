18b1c7c65d072a376e8df521015ecb1b3e6c4726
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-05 19:18:20 +0300

    modal profiilin editoimiselle

--- frontend/src/views/main/Profile.jsx
+++ frontend/src/views/main/Profile.jsx
@@ -8,19 +8,37 @@ import { useUser } from '../../hooks/ApiHooks';
 const Profile = () => {
   const { user } = useContext(UserContext);
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
-  const { deleteUser } = useUser();
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const { deleteUser, updateUser } = useUser();
   const navigate = useNavigate();
+
+  const [editData, setEditData] = useState({
+    username: user ? user.name : '',
+    email: user ? user.email : '',
+    password: '',
+    country: user ? user.countryid : 'FI',
+  });
+
   useEffect(() => {
     if (!user) {
       navigate('/');
     }
   }, [user, navigate]);
+
   const openDeleteModal = () => {
     setDeleteModalOpen(true);
   };
 
   const closeDeleteModal = () => {
     setDeleteModalOpen(false);
+  };
+
+  const openEditModal = () => {
+    setEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalOpen(false);
   };
 
   const handleDeleteProfile = async () => {
@@ -32,14 +50,18 @@ const Profile = () => {
     } catch (error) {
       console.log(error.message);
     }
-
-    // Handle the profile deletion here
-    // You can make an API call to delete the profile
-    // or perform any other necessary actions
-    // After deletion, you can navigate to a different page
-    // or update the user context as needed
-
     closeDeleteModal();
+  };
+
+  const handleEditProfile = async () => {
+    const token = localStorage.getItem('userToken');
+    try {
+      const updateResponse = await updateUser(token, editData);
+      console.log(updateResponse);
+    } catch (error) {
+      console.log(error.message);
+    }
+    closeEditModal();
   };
 
   return (
@@ -60,15 +82,105 @@ const Profile = () => {
             size={240}
           />
           <div className='mt-4 flex justify-between space-x-4'>
-            <button className='button'>
+            <button className='button' onClick={openEditModal}>
               Edit Profile
             </button>
-            <button
-              onClick={openDeleteModal}
-              className='button'
-            >
+            <button onClick={openDeleteModal} className='button'>
               Delete Profile
             </button>
+          </div>
+        </div>
+      )}
+
+      {/* Edit Profile Modal */}
+      {isEditModalOpen && (
+        <div className='fixed inset-0 flex items-center justify-center z-50'>
+          <div className='bg-white w-full md:w-1/2 lg:w-1/3 p-4 rounded-lg shadow-lg'>
+            <h2 className='text-2xl font-semibold mb-4'>Edit Information</h2>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleEditProfile();
+              }}
+              className='flex flex-col'
+            >
+              <label className='flex flex-col items-start mt-2'>
+                <span className='block text-gray-700 font-bold mb-2'>
+                  Username
+                </span>
+                <input
+                  type='text'
+                  value={editData.username}
+                  onChange={(e) =>
+                    setEditData({ ...editData, username: e.target.value })
+                  }
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  aria-label='Username'
+                  autoComplete='username'
+                />
+              </label>
+              <label className='flex flex-col items-start mt-4'>
+                <span className='block text-gray-700 font-bold mb-2'>
+                  Email
+                </span>
+                <input
+                  type='email'
+                  value={editData.email}
+                  onChange={(e) =>
+                    setEditData({ ...editData, email: e.target.value })
+                  }
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  aria-label='Email'
+                  autoComplete='email'
+                />
+              </label>
+              <label className='flex flex-col items-start mt-4'>
+                <span className='block text-gray-700 font-bold mb-2'>
+                  Password
+                </span>
+                <input
+                  type='password'
+                  value={editData.password}
+                  onChange={(e) =>
+                    setEditData({ ...editData, password: e.target.value })
+                  }
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                  aria-label='Password'
+                  autoComplete='new-password'
+                />
+              </label>
+              <label className='flex flex-col items-start mt-4'>
+                <span className='block text-gray-700 font-bold mb-2'>
+                  Country
+                </span>
+                <select
+                  value={editData.country}
+                  onChange={(e) =>
+                    setEditData({ ...editData, country: e.target.value })
+                  }
+                  className='border font-bold border-gray-400 text-white bg-gray-700 rounded-lg p-2 w-full'
+                >
+                  <option value='FI'>Finland</option>
+                  <option value='DK'>Denmark</option>
+                  <option value='NO'>Norway</option>
+                  <option value='SE'>Sweden</option>
+                  <option value='IS'>Iceland</option>
+                  <option value='EE'>Estonia</option>
+                </select>
+              </label>
+              <div className='flex justify-end mt-4'>
+                <button
+                  type='button'
+                  onClick={closeEditModal}
+                  className='button mr-4'
+                >
+                  Cancel
+                </button>
+                <button type='submit' className='button'>
+                  Save
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       )}
@@ -76,7 +188,7 @@ const Profile = () => {
       {/* Delete Profile Modal */}
       {isDeleteModalOpen && (
         <div className='fixed inset-0 flex items-center justify-center z-50'>
-          <div className='bg-white w-1/2 p-4 rounded-lg shadow-lg'>
+          <div className='bg-white w-full md:w-1/2 lg:w-1/3 p-4 rounded-lg shadow-lg'>
             <h2 className='text-2xl font-semibold mb-4'>Confirm Deletion</h2>
             <p>Are you sure you want to delete your profile?</p>
             <div className='mt-4 space-x-4'>
