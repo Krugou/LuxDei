ba91cd7fac7892aa7283d050005c3ff1339933b9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-05 07:51:48 +0300

    Update Logout.jsx

--- frontend/src/views/main/Logout.jsx
+++ frontend/src/views/main/Logout.jsx
@@ -1,14 +1,26 @@
-import React, { useContext, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-import { UserContext } from '../../contexts/UserContext';
+import {useContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {UserContext} from '../contexts/UserContext';
 
-const Logout = (props) => {
-  const { setUser } = useContext(UserContext);
+const Logout = () => {
+  const {setUser} = useContext(UserContext);
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
   useEffect(() => {
     setUser(null);
     localStorage.removeItem('userToken');
-  }, []);
-  return <Navigate to='/' />;
+    setMessage('You have been logged out.');
+    setTimeout(() => {
+      navigate('/');
+    }, 3000);
+  }, [setUser, navigate]);
+
+  return (
+    <div className='text-center'>
+      <p className='text-lg animate-pulse'>{message}</p>
+    </div>
+  );
 };
 
 export default Logout;
