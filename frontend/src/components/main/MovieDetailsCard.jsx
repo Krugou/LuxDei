e24a61884d0c245217e74319ba73c22c3ad7a561
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-06 09:24:13 +0300

    hieman paremmin

--- frontend/src/components/main/MovieDetailsCard.jsx
+++ frontend/src/components/main/MovieDetailsCard.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-const MovieDetailsCard = ({movie}) => {
-    const {title, runningTime, description} = movie;
+const MovieDetailsCard = ({movie, onClose}) => {
+    const runningTime = movie.runningTime;
     const hours = Math.floor(runningTime / 3600000);
     const minutes = Math.floor((runningTime % 3600000) / 60000);
-
     return (
-        <div className="flex flex-col md:flex-row md:space-x-4">
+        <div className="flex flex-col md:flex-row">
             <div className="md:w-1/3">
-                <img src="https://via.placeholder.com/300x450" alt={title} className="w-full" />
+                <img src="https://via.placeholder.com/300x450" alt={movie.title} className="w-full" />
             </div>
-            <div className="md:w-2/3">
-                <h2 className="text-2xl font-bold mb-2">{title}</h2>
+            <div className="md:w-2/3 md:pl-4">
+                <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
+                <p className="text-gray-700 mb-4">{movie.description}</p>
                 <p className="mb-2">Running time: {hours} hours {minutes} minutes</p>
-                <p className="mb-4">{description}</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Buy tickets
-                </button>
+                <button onClick={onClose} className="bg-gray-800 text-white py-2 px-4 rounded-md mt-4">Close</button>
             </div>
         </div>
     );
+};
+
+MovieDetailsCard.propTypes = {
+    movie: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        runningTime: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired
+    }).isRequired,
+    onClose: PropTypes.func.isRequired
 };
 
 export default MovieDetailsCard;
--- frontend/src/views/main/Movies.jsx
+++ frontend/src/views/main/Movies.jsx
@@ -5,6 +5,8 @@ const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
     const [searchQuery, setSearchQuery] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     useEffect(() => {
         // Set movies data from constant array
         const fakeMovies = [
@@ -63,11 +65,16 @@ const Movies = () => {
     }, []);
     const handleMovieSelect = (movie) => {
         setSelectedMovie(movie);
+        setIsModalOpen(true);
+    };
+    const handleModalClose = () => {
+        setSelectedMovie(null);
+        setIsModalOpen(false);
     };
     const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase()));
     return (
         <div className="container mx-auto">
-            <h1 className="text-3xl font-bold mb-4">Movies List</h1>
+            <h1 className="text-3xl font-bold mb-4">Movie List</h1>
             <input type="text" placeholder="Search movies..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="border border-gray-400 rounded-md px-4 py-2 mb-4" />
             <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {filteredMovies.map(movie => (
@@ -79,7 +86,21 @@ const Movies = () => {
                     </li>
                 ))}
             </ul>
-            {selectedMovie && <MovieDetailsCard movie={selectedMovie} />}
+            {isModalOpen && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center" onClick={handleModalClose}>
+                    <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+                    <div className="bg-white rounded-lg shadow-lg z-10 overflow-y-auto" onClick={handleModalClose}>
+                        <div className="p-4">
+                            <button onClick={handleModalClose} className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-700 focus:outline-none">
+                                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                                    <path d="M18.3 5.7c-.4-.4-1-.4-1.4 0L12 10.6 7.1 5.7c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4L10.6 12l-4.9 4.9c-.4.4-.4 1 0 1.4.2.2.5.3.7.3s.5-.1.7-.3L12 13.4l4.9 4.9c.2.2.5.3.7.3s.5-.1.7-.3c.4-.4.4-1 0-1.4L13.4 12l4.9-4.9c.4-.4.4-1 0-1.4z" />
+                                </svg>
+                            </button>
+                            <MovieDetailsCard movie={selectedMovie} onClose={handleModalClose} />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
