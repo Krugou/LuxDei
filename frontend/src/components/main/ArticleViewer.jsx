6b4650058adaae2e7a04ed675b39840487a36473
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-06 13:50:54 +0300

    Create ArticleViewer.jsx

---
+++ frontend/src/components/main/ArticleViewer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const ArticleViewer = ({articles}) => {
+
+
+    return (
+        <div className='bg-gray-100 p-8 mx-auto px-4 sm:px-6 lg:px-8'>
+            <h1 className='text-3xl font-bold text-center mt-8 mb-4'>Articles</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {articles.map((article) => (
+                    <div key={article.id} className='bg-white rounded-lg shadow-lg mb-10 overflow-hidden'>
+                        <div className='p-4'>
+                            <h2 className='text-xl font-bold mb-2' id={`article-title-${article.id}`} aria-label='Article title'>
+                                {article.title}
+                            </h2>
+                            <p className='text-gray-700 text-base' id={`article-content-${article.id}`} aria-label='Article content'>
+                                {article.content}
+                            </p>
+                            <div className='text-gray-500 text-sm mt-2' id={`article-timestamp-${article.id}`} aria-label='Article timestamp'>
+                                {new Date(article.timestamp).toLocaleString()}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+ArticleViewer.PropTypes = {
+    articles: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+            timestamp: PropTypes.string.isRequired
+        })
+    ).isRequired
+};
+
+export default ArticleViewer;
