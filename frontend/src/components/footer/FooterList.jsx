a80c0050435301df196a2433a88c849811cd5a64
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-06 09:42:55 +0300

    Update FooterList.jsx

--- frontend/src/components/footer/FooterList.jsx
+++ frontend/src/components/footer/FooterList.jsx
@@ -9,18 +9,23 @@ const FooterList = () => {
                 <FooterListButton name="home" />
                 <FooterListButton name="about" />
                 <FooterListButton name="articles" />
+                <FooterListButton name="livestream" />
+
             </ul>
 
             <ul className="flex flex-col">
                 <FooterListButton name="login" />
                 <FooterListButton name="register" />
                 <FooterListButton name="team" />
+                <FooterListButton name="movies" />
 
             </ul>
             <ul className="flex flex-col ">
                 <FooterListButton name="terms of service" />
                 <FooterListButton name="privacy policy" />
                 <FooterListButton name="schedule" />
+                <FooterListButton name="profile" />
+                
                 
 
             </ul>
