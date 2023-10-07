59f4b7362aeda3e0d8229dbd67a45d6820e26f02
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-07 18:37:14 +0300

    Update LiveStreamButton.jsx

--- frontend/src/components/header/buttons/LiveStreamButton.jsx
+++ frontend/src/components/header/buttons/LiveStreamButton.jsx
@@ -1,19 +1,35 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-const LiveStreamButton = ({name, navigate, lastItem = false, }) => {
+import React, {useState, useEffect} from 'react';
+
+const LiveStreamButton = ({name, navigate, lastItem = false}) => {
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    const checkOnlineStatus = async () => {
+      try {
+        const response = await fetch('http://195.148.104.124:1935/jakelu/jakfilms/manifest.mpd');
+        setIsOnline(response.ok);
+      } catch (error) {
+        setIsOnline(false);
+      }
+    };
+    checkOnlineStatus();
+  }, []);
 
   return (
-      <li>
-        <button
-            className={` nav-button ${lastItem ? 'mx-2' : 'md:mx-5'} text-lg rounded bg-gmgold p-1 font-bold transition-all hover:text-gray-700 hover:text-xl mx-2`}
-            onClick={() => {
-              navigate(`/${name}`);
-            }}
-        >Livestream
-        </button>
-      </li>
+    <li>
+      <button
+        className={`nav-button ${lastItem ? 'mx-2' : 'md:mx-5'} text-lg rounded bg-gmgold p-1 font-bold transition-all hover:text-gray-700 hover:text-xl mx-2 ${isOnline ? 'animate-pulse' : ''}`}
+        onClick={() => {
+          navigate(`/${name}`);
+        }}
+      >
+        Livestream
+      </button>
+    </li>
   );
 };
+
 LiveStreamButton.propTypes = {
   name: PropTypes.string.isRequired,
   navigate: PropTypes.func.isRequired,
