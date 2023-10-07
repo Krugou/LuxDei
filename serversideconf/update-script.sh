17b6992f2a7c3d42e25cbc72bafa19a9bb7001d8
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-07 14:16:59 +0300

    Update update-script.sh

--- serversideconf/update-script.sh
+++ serversideconf/update-script.sh
@@ -8,7 +8,7 @@ eval `ssh-agent -s`
 ssh-add /home/aanderson/.ssh/id_ed25519
 
 # Perform a Git pull to update the code
-git pull --rebase
+git pull 
 
 # Check if changes were pulled (git diff will show changes) 
 if [ -n "$(git diff)" ]; then
