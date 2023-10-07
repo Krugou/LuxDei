6170da00c2fdae571486d728cdbddfcf52369cdb
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-07 14:24:42 +0300

    Update update-script.sh

--- serversideconf/update-script.sh
+++ serversideconf/update-script.sh
@@ -8,10 +8,9 @@ eval `ssh-agent -s`
 ssh-add /home/aanderson/.ssh/id_ed25519
 
 # Perform a Git pull to update the code
-git pull 
-
-# Check if changes were pulled (git diff will show changes) 
-if [ -n "$(git diff)" ]; then
+if git pull | grep -q 'Already up to date.'; then
+  echo 'No changes pulled.'
+else
   # If changes were pulled, navigate to the frontend directory
   cd frontend
   npm install
