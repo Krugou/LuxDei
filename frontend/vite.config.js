ed60729c164494c2d9b376db1404afb3424a501c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-08 13:32:11 +0300

    vite options change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: true,
+    terserOptions: {
+      compress: {
+        // drop_console: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
+});
