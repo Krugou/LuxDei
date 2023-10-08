2344bc1164167951d11c4a1bf846c2b3838ba1e9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-08 11:37:57 +0300

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: "FileServer",
     script: "./jakfilmsserver.js",
     watch: true,
     min_uptime: 10000,
+    ignore_watch: ["node_modules", "logs", "package-lock.json",]
   },
   {
-    name   : "ChatDatabase",
+    name: "ChatDatabase",
     script: "./jakfilmsData.js",
     watch: true,
     min_uptime: 10000,
+    ignore_watch: ["node_modules", "logs", "package-lock.json",]
 
   }]
-}
+};
