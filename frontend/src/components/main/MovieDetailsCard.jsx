bd778fe06ac46b4062fe53ed8a4831747cf717a4
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-09 17:31:42 +0300

    Update MovieDetailsCard.jsx

--- frontend/src/components/main/MovieDetailsCard.jsx
+++ frontend/src/components/main/MovieDetailsCard.jsx
@@ -1,68 +1,71 @@
 import PropTypes from 'prop-types';
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {UserContext} from '../../contexts/UserContext';
 import LoadingSpinner from '../LoadingSpinner';
-
+import LikesElement from './LikesElement';
 
 const MovieDetailsCard = ({movie, onClose, loadStatus}) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const runningTime = movie.runningTime;
-    const hours = Math.floor(runningTime / 3600000);
-    const minutes = Math.floor((runningTime % 3600000) / 60000);
-    const seconds = Math.floor((runningTime % 60000) / 1000);
+	const {user} = useContext(UserContext);
+	const [isLoading, setIsLoading] = useState(true);
+	const runningTime = movie.runningTime;
+	const hours = Math.floor(runningTime / 3600000);
+	const minutes = Math.floor((runningTime % 3600000) / 60000);
+	const seconds = Math.floor((runningTime % 60000) / 1000);
 
-    useEffect(() => {
-        setIsLoading(false);
-    }, []);
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
 
-   
-
-    return (
-        <>
-            {isLoading ||  loadStatus === 'loading' ? (
-                <LoadingSpinner />
-            ) : (
-                <div className='flex flex-col md:flex-row'>
-                    <div className='md:w-1/3'>
-                        <img
-                            src={movie.img}
-                            alt={movie.title}
-                            className='w-full'
-                        />
-                    </div>
-                    <div className='md:w-2/3 md:pl-4'>
-                        <h2 className='text-2xl font-bold mb-2'>{movie.title}</h2>
-                        <p className='text-gray-700 mb-4'>{movie.description}</p>
-                        {runningTime > 0 && (
-                            <p className='mb-2'>
-                                Running time:
-                                {hours > 0 && ` ${hours} hours`}
-                                {minutes > 0 && ` ${minutes} minutes`}
-                                {seconds > 0 && ` ${seconds} seconds`}
-                            </p>
-                        )}
-                        <button
-                            onClick={onClose}
-                            className='bg-gray-800 text-white py-2 px-4 rounded-md mt-4'
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+	return (
+		<>
+			{isLoading || loadStatus === 'loading' ? (
+				<LoadingSpinner />
+			) : (
+				<div className='flex flex-col md:flex-row'>
+					<div className='md:w-1/3'>
+						<img
+							src={movie.img}
+							alt={movie.title}
+							className='w-full'
+						/>
+					</div>
+					<div className='md:w-2/3 md:pl-4'>
+						<h2 className='text-2xl font-bold mb-2'>{movie.title}</h2>
+						<p className='text-gray-700 mb-4'>{movie.description}</p>
+						{runningTime > 0 && (
+							<p className='mb-2'>
+								Running time:
+								{hours > 0 && ` ${hours} hours`}
+								{minutes > 0 && ` ${minutes} minutes`}
+								{seconds > 0 && ` ${seconds} seconds`}
+							</p>
+						)}
+						<LikesElement
+							user={user}
+							location={movie.title}
+						/>
+						<button
+							onClick={onClose}
+							className='bg-gray-800 text-white py-2 px-4 rounded-md mt-4'>
+							Close
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 MovieDetailsCard.propTypes = {
-    movie: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        runningTime: PropTypes.number.isRequired,
-        description: PropTypes.string.isRequired,
-        img: PropTypes.string.isRequired,
-    }).isRequired,
-    onClose: PropTypes.func.isRequired,
-    loadStatus: PropTypes.oneOf(['loading', 'loaded']).isRequired,
+	movie: PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		title: PropTypes.string.isRequired,
+		runningTime: PropTypes.number.isRequired,
+		description: PropTypes.string.isRequired,
+		img: PropTypes.string.isRequired,
+	}).isRequired,
+	onClose: PropTypes.func.isRequired,
+	loadStatus: PropTypes.oneOf(['loading', 'loaded']).isRequired,
 };
 
 export default MovieDetailsCard;
