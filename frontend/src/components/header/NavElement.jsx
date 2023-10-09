e2587e962f49bc78d7c7ea8af47c039cfa29284e
KaarleH <kaarleh@metropolia.fi>
2023-10-09 19:32:49 +0300

    navUpdate

--- frontend/src/components/header/NavElement.jsx
+++ frontend/src/components/header/NavElement.jsx
@@ -1,30 +1,30 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../contexts/UserContext';
-import { useUser } from '../../hooks/ApiHooks';
+import React, {useContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {UserContext} from '../../contexts/UserContext';
+import {useUser} from '../../hooks/ApiHooks';
 import HeaderTitle from './HeaderTitle';
 import HeaderListButton from './buttons/HeaderListButton';
 import WeatherData from './weather/WeatherData';
-import { VideoFeedContext } from '../../contexts/VideoFeedContext';
-
+import {VideoFeedContext} from '../../contexts/VideoFeedContext';
 const NavElement = () => {
   const navigate = useNavigate();
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const { user, setUser } = useContext(UserContext);
-  const { getUserInfoByToken } = useUser();
+  const {user, setUser} = useContext(UserContext);
+  const {getUserInfoByToken} = useUser();
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
   const [position, setPosition] = useState(null);
-  const { isVideoFeedOnline } = useContext(VideoFeedContext);
+  const {isVideoFeedOnline} = useContext(VideoFeedContext);
 
   const getUserInfo = async () => {
     const userToken = localStorage.getItem('userToken');
     if (userToken) {
       const user = await getUserInfoByToken(userToken);
 
+      // console.log(user, 'userinfomaan');
       if (user) {
         setUser(user);
         const target = location.pathname === '/' ? '/' : location.pathname;
@@ -36,69 +36,71 @@ const NavElement = () => {
   };
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setPosition({ lat: latitude, lon: longitude });
-        },
-        (error) => {
-          console.error(error);
-          setDefaultCoords();
-        }
+      (position) => {
+        const {latitude, longitude} = position.coords;
+        // console.log('Latitude is :', latitude);
+        // console.log('Longitude is :', longitude);
+        setPosition({lat: latitude, lon: longitude});
+      },
+      (error) => {
+        console.error(error);
+        setDefaultCoords();
+      }
     );
   }, []);
   const setDefaultCoords = () => {
-    setPosition({ lat: 60.2052, lon: 24.6564 }); // Default coordinates for Espoo
+    setPosition({lat: 60.2052, lon: 24.6564}); // Default coordinates for Espoo
   };
   useEffect(() => {
     getUserInfo();
-  }, []);
+  }, []); // jos taulukko tyhjä, ajetaan vain kerran
 
   return (
-      <nav className='p-4 bg-gmdeepblack border-b-4 border-gmgold  sticky top-0 w-full z-10'>
-        <div className='container mx-auto flex justify-between items-center relative'>
-          <HeaderTitle />
+    <nav className='p-4 bg-gmdeepblack border-b-4 border-gmgold  sticky top-0 w-full z-10'>
+      <div className='container mx-auto flex justify-between items-center relative'>
+        <HeaderTitle />
 
-          {position && !isNavOpen && <WeatherData coords={position} />}
+        {position && !isNavOpen && <WeatherData coords={position} />}
 
-          <ul
-              id='nav-links'
-              className={` md:flex flex-row ${isNavOpen ? 'flex' : 'hidden '}`}
-          >
-            {user && user.userrole === 0 ? (
-                <>
-                  <HeaderListButton name='admin' navigate={navigate} />
-                  <HeaderListButton name='profile' navigate={navigate} />
-                  <HeaderListButton name='logout' navigate={navigate} />
-                </>
-            ) : user ? (
-                <>
-                  <HeaderListButton name='profile' navigate={navigate} />
-                  <HeaderListButton name='logout' navigate={navigate} />
-                </>
-            ) : (
-                <>
-                  <HeaderListButton name='login' navigate={navigate} />
-                  <HeaderListButton name='register' navigate={navigate} />
-                </>
-            )}
+        <ul
+          id='nav-links'
+          className={` md:flex flex-row ${isNavOpen ? 'flex' : 'hidden '}`}
+        >
+          {user && user.userrole === 0 ? (
+            <>
+              <HeaderListButton name='admin' navigate={navigate} />
+              <HeaderListButton name='profile' navigate={navigate} />
+              <HeaderListButton name='logout' navigate={navigate} />
+            </>
+          ) : user ? (
+            <>
+              <HeaderListButton name='profile' navigate={navigate} />
+              <HeaderListButton name='logout' navigate={navigate} />
+            </>
+          ) : (
+            <>
+              <HeaderListButton name='login' navigate={navigate} />
+              <HeaderListButton name='register' navigate={navigate} />
+            </>
+          )}
 
-            <HeaderListButton name='livestream' navigate={navigate} isOnline={isVideoFeedOnline} />
-            <HeaderListButton name='movies' navigate={navigate} />
-            <HeaderListButton
-                name='articles'
-                navigate={navigate}
-                lastItem={true}
-            />
-          </ul>
-          <div
-              id='mobile-menu-button'
-              className='text-white md:hidden cursor-pointer'
-              onClick={toggleNav}
-          >
-            {isNavOpen ? <MenuOpenIcon /> : <MenuIcon />}
-          </div>
+          <HeaderListButton name='livestream' navigate={navigate} isOnline={isVideoFeedOnline} />
+          <HeaderListButton name='movies' navigate={navigate} />
+          <HeaderListButton
+            name='articles'
+            navigate={navigate}
+            lastItem={true}
+          />
+        </ul>
+        <div
+          id='mobile-menu-button'
+          className='text-white md:hidden cursor-pointer'
+          onClick={toggleNav}
+        >
+          {isNavOpen ? <MenuOpenIcon /> : <MenuIcon />}
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
