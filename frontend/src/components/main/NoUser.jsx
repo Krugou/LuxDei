693823776eec7f39b48c40577f406778d508c19e
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-09 22:50:40 +0300

    Update NoUser.jsx

--- frontend/src/components/main/NoUser.jsx
+++ frontend/src/components/main/NoUser.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
-const NoUser = () => {
-  const navigate = useNavigate();
+const NoUser = (name) => {
+	const navigate = useNavigate();
 
-  return (
-    <div className='flex flex-col items-center justify-center'>
-      <h1 className='lg:text-xl text-base m-2 font-bold mb-8'>
-        Please login or register to chat
-      </h1>
-      <div className='flex flex-col items-center'>
-        <button className='button mb-5' onClick={() => navigate('/login')}>
-          Login
-        </button>
-        <button className='button' onClick={() => navigate('/register')}>
-          Register
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center justify-center'>
+			<h1 className='lg:text-xl text-base m-2 font-bold mb-8'>
+				Please login or register to {name}
+			</h1>
+			<div className='flex flex-col items-center'>
+				<button
+					className='button mb-5'
+					onClick={() => navigate('/login')}>
+					Login
+				</button>
+				<button
+					className='button'
+					onClick={() => navigate('/register')}>
+					Register
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default NoUser;
