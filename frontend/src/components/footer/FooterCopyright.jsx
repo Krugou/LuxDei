7a135832549ee49ff9c7ad60385682fcb2ef7c49
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-10 02:09:52 +0300

    footterin kokoa pienemmäksi

--- frontend/src/components/footer/FooterCopyright.jsx
+++ frontend/src/components/footer/FooterCopyright.jsx
@@ -2,16 +2,21 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const FooterCopyright = (props) => {
-    const {companyName} = props;
+  const { companyName } = props;
 
-    return (
-        <p className="text-center text-gray-500 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl" aria-label={`Copyright ${new Date().getFullYear()} ${companyName}`}
-            title={`Copyright ${new Date().getFullYear()} ${companyName}`}>&copy; {new Date().getFullYear()} {companyName}</p>
-    );
+  return (
+    <p
+      className='text-center text-gray-500 text-sm border-t border-white sm:text-base md:text-lg lg:text-xl'
+      aria-label={`Copyright ${new Date().getFullYear()} ${companyName}`}
+      title={`Copyright ${new Date().getFullYear()} ${companyName}`}
+    >
+      &copy; {new Date().getFullYear()} {companyName}
+    </p>
+  );
 };
 
 FooterCopyright.propTypes = {
-    companyName: PropTypes.string.isRequired,
+  companyName: PropTypes.string.isRequired,
 };
 
 export default FooterCopyright;
--- frontend/src/components/footer/buttons/FooterListButton.jsx
+++ frontend/src/components/footer/buttons/FooterListButton.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const FooterListButton = ({name}) => {
-    const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
-    name = name.replace(/\s+/g, '');
-    const navigate = useNavigate();
-    return (
-        <li className='p-2'>
-            <button
-                onClick={() => {
-                    navigate(`/${name}`);
-                }}
-                className='mx-3 text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl text-white transition hover:text-gray-300'
-                aria-label={`Go to ${capitalized}`}
-                title={`Go to ${capitalized}`}
-            >
-                {capitalized}
-            </button>
-        </li>
-    );
+const FooterListButton = ({ name }) => {
+  const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+  name = name.replace(/\s+/g, '');
+  const navigate = useNavigate();
+  return (
+    <li className='p-2'>
+      <button
+        onClick={() => {
+          navigate(`/${name}`);
+        }}
+        className='mx-3 text-sm sm:text-md md:text-lg lg:text-xl text-white transition hover:text-gray-300'
+        aria-label={`Go to ${capitalized}`}
+        title={`Go to ${capitalized}`}
+      >
+        {capitalized}
+      </button>
+    </li>
+  );
 };
 
 FooterListButton.propTypes = {
-    name: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default FooterListButton;
