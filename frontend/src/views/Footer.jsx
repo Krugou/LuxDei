ba752a742ee34c541d478b615c5ff28f0f285afd
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-10 02:12:22 +0300

    Update Footer.jsx

--- frontend/src/views/Footer.jsx
+++ frontend/src/views/Footer.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import FooterList from '../components/footer/FooterList';
 import FooterCopyright from '../components/footer/FooterCopyright';
 const Footer = () => {
-
-
-    return (
-        <footer className="bg-gmdeepblack border-t-4 border-gmgold text-gray-400 py-4">
-            <FooterList />
-            <FooterCopyright
-                companyName="JakFilms"
-            />
-        </footer>
-    );
+  return (
+    <footer className='bg-gmdeepblack border-t-4 border-gmgold text-gray-400 py-4'>
+      <FooterList />
+      <FooterCopyright companyName='Jak Films Productions' />
+    </footer>
+  );
 };
 
 export default Footer;
