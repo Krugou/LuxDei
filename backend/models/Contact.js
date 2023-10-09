09221a62c2c6149f63d12f7fa571d063deb6c1cb
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-09 12:32:24 +0300

    Update Contact.js

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // Define the Contact schema
 const contactSchema = new mongoose.Schema({
@@ -23,4 +23,4 @@ const contactSchema = new mongoose.Schema({
 // Create a Mongoose model using the schema
 const Contact = mongoose.model('Contact', contactSchema);
 
-module.exports = Contact;
+export default Contact;
