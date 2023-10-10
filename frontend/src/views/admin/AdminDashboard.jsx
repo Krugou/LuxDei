f2784dd2dcfdc866ada8a94bd94bce90d74790ea
KaarleH <kaarleh@metropolia.fi>
2023-10-10 23:09:03 +0300

    adminDashboard

--- frontend/src/views/admin/AdminDashboard.jsx
+++ frontend/src/views/admin/AdminDashboard.jsx
@@ -119,8 +119,8 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
-      <div className='relative py-3 sm:max-w-xl md:max-w-full sm:mx-auto'>
+    <div className='min-h-screen p-12 bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
+      <div className='relative py-3 max-h-96 sm:max-w-xl md:max-w-full sm:mx-auto'>
         <div className='absolute inset-0 bg-gradient-to-r from-yellow-400 to-indigo-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl'></div>
         <div className='relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20'>
           <h1 className='text-3xl font-bold text-gray-900 mb-8 border-b border-black '>
