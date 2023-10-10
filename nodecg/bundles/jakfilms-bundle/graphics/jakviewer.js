bf684cf703fa5e3418d7e1f329ec9eaf3930bc00
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-10 17:02:17 +0300

    Update jakviewer.js

--- nodecg/bundles/jakfilms-bundle/graphics/jakviewer.js
+++ nodecg/bundles/jakfilms-bundle/graphics/jakviewer.js
@@ -1,171 +1,180 @@
 // You can access the NodeCG api anytime from the `window.nodecg` object
 // Or just `nodecg` for short. Like this!:
 nodecg.log.info("Here's an example of using NodeCG's logging API!");
-const speakerRep = nodecg.Replicant("speakerReplicant");
-const roleRep = nodecg.Replicant("roleReplicant");
-const broadcasterRep = nodecg.Replicant("broadcasterReplicant");
-const broadcastRep = nodecg.Replicant("broadcastReplicant");
-const blokki = document.getElementById("blokki");
-const puhuja = document.getElementById("puhuja");
-const tokablokki = document.getElementById("tokablokki");
-const kolmasblokki = document.getElementById("kolmasblokki");
-const lahetys = document.getElementById("lahetys");
-const lahetystyyppi = document.getElementById("lahetystyyppi");
-const laskuri = document.getElementById("laskuri")
-const rooli = document.getElementById("rooli");
-const isToggled = nodecg.Replicant("isToggled");
-const isToggled2 = nodecg.Replicant("isToggled2");
-const isToggled3 = nodecg.Replicant("isToggled3");
-const lowerthird = document.getElementById("lowerThird");
-const lowersecond = document.getElementById("lowerSecond");
-const lowerfourth = document.getElementById("lowerFourth");
-const timerRep = nodecg.Replicant("timerReplicant");
-const countdownDisplay = document.getElementById("laskuri");
-
+const speakerRep = nodecg.Replicant('speakerReplicant');
+const roleRep = nodecg.Replicant('roleReplicant');
+const broadcasterRep = nodecg.Replicant('broadcasterReplicant');
+const broadcastRep = nodecg.Replicant('broadcastReplicant');
+const blokki = document.getElementById('blokki');
+const puhuja = document.getElementById('puhuja');
+const tokablokki = document.getElementById('tokablokki');
+const kolmasblokki = document.getElementById('kolmasblokki');
+const lahetys = document.getElementById('lahetys');
+const lahetystyyppi = document.getElementById('lahetystyyppi');
+const laskuri = document.getElementById('laskuri');
+const rooli = document.getElementById('rooli');
+const isToggled = nodecg.Replicant('isToggled');
+const isToggled2 = nodecg.Replicant('isToggled2');
+const isToggled3 = nodecg.Replicant('isToggled3');
+const lowerthird = document.getElementById('lowerThird');
+const lowersecond = document.getElementById('lowerSecond');
+const lowerfourth = document.getElementById('lowerFourth');
+const timerRep = nodecg.Replicant('timerReplicant');
+const countdownDisplay = document.getElementById('laskuri');
 
 console.log(isToggled.value);
 
-isToggled.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  if (newValue === true) {
-    console.log("true");
-    lowerthird.classList.remove("hidden");
-  } else if (newValue === false) {
-    console.log("false");
-    lowerthird.classList.add("hidden");
-  }
+isToggled.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	if (newValue === true) {
+		console.log('true');
+		lowerthird.classList.remove('hidden');
+	} else if (newValue === false) {
+		console.log('false');
+		lowerthird.classList.add('hidden');
+	}
 });
 
-isToggled2.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  if (newValue === true) {
-    console.log("true");
-    lowersecond.classList.remove("hidden");
-  } else if (newValue === false) {
-    console.log("false");
-    lowersecond.classList.add("hidden");
-  }
+isToggled2.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	if (newValue === true) {
+		console.log('true');
+		lowersecond.classList.remove('hidden');
+	} else if (newValue === false) {
+		console.log('false');
+		lowersecond.classList.add('hidden');
+	}
 });
 
-isToggled3.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  if (newValue === true) {
-    console.log("true");
-    lowerfourth.classList.remove("hidden");
-  } else if (newValue === false) {
-    console.log("false");
-    lowerfourth.classList.add("hidden");
-  }
+isToggled3.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	if (newValue === true) {
+		console.log('true');
+		lowerfourth.classList.remove('hidden');
+	} else if (newValue === false) {
+		console.log('false');
+		lowerfourth.classList.add('hidden');
+	}
 });
 
-
-timerRep.on("change", (newValue, oldValue) => {
-  countdownDisplay.textContent = newValue;
+timerRep.on('change', (newValue, oldValue) => {
+	countdownDisplay.textContent = newValue;
 });
-
 
 let startTime;
 let endTime = null;
 
+timerRep.on('change', (newValue, oldValue) => {
+	const [hours, minutes, seconds] = newValue.split(':').map(Number);
+	const currentTime = new Date().getTime();
+	endTime = currentTime + (hours * 3600 + minutes * 60 + seconds) * 1000;
 
-timerRep.on("change", (newValue, oldValue) => {
-  const [hours, minutes, seconds] = newValue.split(":").map(Number);
-  const currentTime = new Date().getTime();
-  endTime = currentTime + (hours * 3600 + minutes * 60 + seconds) * 1000;
-
-  updateCountdown();
+	updateCountdown();
 });
 
-function updateCountdown() {
-  if (endTime === null) {
-    countdownDisplay.textContent = "00:00:00";
-    return;
-  }
+const updateCountdown = () => {
+	if (endTime === null) {
+		countdownDisplay.textContent = '00:00:00';
+		return;
+	}
 
-  const currentTime = new Date().getTime();
-  const timeRemaining = endTime - currentTime;
+	const currentTime = new Date().getTime();
+	const timeRemaining = endTime - currentTime;
 
-  if (timeRemaining <= 0) {
-    countdownDisplay.textContent = "00:00:00";
-  } else {
-    const hours = Math.floor(timeRemaining / 3600000);
-    const minutes = Math.floor((timeRemaining % 3600000) / 60000);
-    const seconds = Math.floor((timeRemaining % 60000) / 1000);
-    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    countdownDisplay.textContent = formattedTime;
+	if (timeRemaining <= 0) {
+		countdownDisplay.textContent = 'Time is up!';
+	} else {
+		const hours = Math.floor(timeRemaining / 3600000);
+		const minutes = Math.floor((timeRemaining % 3600000) / 60000);
+		const seconds = Math.floor((timeRemaining % 60000) / 1000);
 
-    setTimeout(updateCountdown);
-  }
-}
+		let formattedTime = '';
+		if (hours > 0) {
+			formattedTime += `${String(hours).padStart(2, '0')}:`;
+		}
+		if (minutes > 0 || hours > 0) {
+			formattedTime += `${String(minutes).padStart(2, '0')}:`;
+		}
+		formattedTime += `${String(seconds).padStart(2, '0')}`;
+
+		countdownDisplay.textContent = formattedTime;
+
+		setTimeout(updateCountdown);
+	}
+};
 
 updateCountdown();
 
-speakerRep.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  puhuja.innerHTML = newValue;
-  animations();
+speakerRep.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	puhuja.innerHTML = newValue;
+	animations();
 });
-roleRep.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  rooli.innerHTML = newValue;
-  animations();
+roleRep.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	rooli.innerHTML = newValue;
+	animations();
 });
 
-broadcasterRep.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  lahetys.innerHTML = newValue;
-  broadcastAnimations();
+broadcasterRep.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	lahetys.innerHTML = newValue;
+	broadcastAnimations();
 });
-broadcastRep.on("change", (newValue, oldValue) => {
-  console.log(`myRep changed from ${oldValue} to ${newValue}`);
-  lahetystyyppi.innerHTML = newValue;
-  broadcastAnimations();
+broadcastRep.on('change', (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	lahetystyyppi.innerHTML = newValue;
+	broadcastAnimations();
 });
-timerRep.on("change", (newValue, oldValue) => {
-  countdownDisplay.textContent = newValue;
-  timerAnimations()
+timerRep.on('change', (newValue, oldValue) => {
+	countdownDisplay.textContent = newValue;
+	timerAnimations();
 });
 
 const animations = () => {
+	puhuja.classList.remove('title-h1-animation');
+	puhuja.offsetWidth;
+	puhuja.classList.add('title-h1-animation');
 
-  puhuja.classList.remove("title-h1-animation");
-  puhuja.offsetWidth;
-  puhuja.classList.add("title-h1-animation");
+	tokablokki.classList.remove('role-block-animation');
+	tokablokki.offsetWidth;
+	tokablokki.classList.add('role-block-animation');
+	tokablokki.classList.remove('hidden');
 
-  tokablokki.classList.remove("role-block-animation");
-  tokablokki.offsetWidth;
-  tokablokki.classList.add("role-block-animation");
-  tokablokki.classList.remove("hidden");
-
-
-  rooli.classList.remove("role-p-animation");
-  rooli.offsetWidth;
-  rooli.classList.add("role-p-animation");
+	rooli.classList.remove('role-p-animation');
+	rooli.offsetWidth;
+	rooli.classList.add('role-p-animation');
 };
 
 const timerAnimations = () => {
-  kolmasblokki.classList.remove("role-block-animation");
-  kolmasblokki.offsetWidth;
-  kolmasblokki.classList.add("role-block-animation");
-  kolmasblokki.classList.remove("hidden");
+	kolmasblokki.classList.remove('role-block-animation');
+	kolmasblokki.offsetWidth;
+	kolmasblokki.classList.add('role-block-animation');
+	kolmasblokki.classList.remove('hidden');
 
-
-  laskuri.classList.remove("role-p-animation");
-  laskuri.offsetWidth;
-  laskuri.classList.add("role-p-animation");
-}
+	laskuri.classList.remove('role-p-animation');
+	laskuri.offsetWidth;
+	laskuri.classList.add('role-p-animation');
+};
 const broadcastAnimations = () => {
-  blokki.classList.remove("title-block-animation");
-  blokki.offsetWidth;
-  blokki.classList.add("title-block-animation");
-  blokki.classList.remove("hidden");
+	blokki.classList.remove('title-block-animation');
+	blokki.offsetWidth;
+	blokki.classList.add('title-block-animation');
+	blokki.classList.remove('hidden');
 
-  lahetys.classList.remove("title-h1-animation");
-  lahetys.offsetWidth;
-  lahetys.classList.add("title-h1-animation");
+	lahetys.classList.remove('title-h1-animation');
+	lahetys.offsetWidth;
+	lahetys.classList.add('title-h1-animation');
 
-  lahetystyyppi.classList.remove("role-p-animation");
-  lahetystyyppi.offsetWidth;
-  lahetystyyppi.classList.add("role-p-animation");
+	lahetystyyppi.classList.remove('role-p-animation');
+	lahetystyyppi.offsetWidth;
+	lahetystyyppi.classList.add('role-p-animation');
+};
 
-}
+const StartSettingsHidden = () => {
+	lowerthird.classList.add('hidden');
+	lowersecond.classList.add('hidden');
+	lowerfourth.classList.add('hidden');
+};
+
+StartSettingsHidden();
