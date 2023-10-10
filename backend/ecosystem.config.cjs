ca3c3f4f42a43797cad06b761928bf9e46d494a2
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-10 08:16:47 +0300

    Update ecosystem.config.cjs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,17 +1,20 @@
 module.exports = {
-  apps: [{
-    name: "FileServer",
-    script: "./jakfilmsserver.js",
-    watch: true,
-    min_uptime: 10000,
-    ignore_watch: ["node_modules", "logs", "package-lock.json",]
-  },
-  {
-    name: "ChatDatabase",
-    script: "./jakfilmsData.js",
-    watch: true,
-    min_uptime: 10000,
-    ignore_watch: ["node_modules", "logs", "package-lock.json","jakfilms"]
-
-  }]
+	apps: [
+		{
+			name: 'FileServer',
+			script: './jakfilmsserver.js',
+			watch: true,
+			min_uptime: 10000,
+			ignore_watch: ['node_modules', 'logs', 'package-lock.json'],
+			watch_delay: 3000,
+		},
+		{
+			name: 'ChatDatabase',
+			script: './jakfilmsData.js',
+			watch: true,
+			min_uptime: 10000,
+			ignore_watch: ['node_modules', 'logs', 'package-lock.json', 'jakfilms'],
+			watch_delay: 3000,
+		},
+	],
 };
