31d71b6a70ada1b918233d59055e945607677c07
KaarleH <kaarleh@metropolia.fi>
2023-10-11 01:49:39 +0300

    update

--- frontend/src/views/main/Team.jsx
+++ frontend/src/views/main/Team.jsx
@@ -11,19 +11,19 @@ const Team = () => {
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mt-6">
                 <TeamMember
-                    imgSrc="./img/grim.png"
+                    imgSrc="../images/Joonas.PNG"
                     altText="Team Member 1"
                     name="Joonas Grim"
                     role="Festival Director"
                 />
                 <TeamMember
-                    imgSrc="./img/dying.png"
+                    imgSrc="../images/aleksi.PNG"
                     altText="Team Member 2"
                     name="Aleksi Nightshade"
                     role="Program Coordinator"
                 />
                 <TeamMember
-                    imgSrc="./img/stanley.png"
+                    imgSrc="../images/Kaarle.PNG"
                     altText="Team Member 3"
                     name="Kaarle Darkwood"
                     role="Marketing Manager"
