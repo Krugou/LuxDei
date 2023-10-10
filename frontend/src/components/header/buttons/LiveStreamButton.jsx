ceca44d32b5b31150f786baeef0538f725bd1e29
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-10 18:54:48 +0300

    Update LiveStreamButton.jsx

--- frontend/src/components/header/buttons/LiveStreamButton.jsx
+++ frontend/src/components/header/buttons/LiveStreamButton.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
-import React, {useContext, useEffect, useState} from 'react';
-import {VideoFeedContext} from '../../../contexts/VideoFeedContext';
-const LiveStreamButton = ({name, navigate, lastItem = false}) => {
-  const {isVideoFeedOnline} = useContext(VideoFeedContext);
-  
+import React, { useContext, useEffect, useState } from 'react';
+import { VideoFeedContext } from '../../../contexts/VideoFeedContext';
+const LiveStreamButton = ({ name, navigate, lastItem = false }) => {
+  const { isVideoFeedOnline } = useContext(VideoFeedContext);
 
   return (
     <li>
       <button
-        className={`nav-button ${lastItem ? 'mx-2' : 'md:mx-5'} text-lg rounded bg-gmgold p-1 font-bold transition-all hover:text-gray-700 hover:text-xl mx-2 ${isVideoFeedOnline ? 'animate-pulse' : ''}`}
+        className={`nav-button ${
+          lastItem ? 'mx-2 ' : 'md:mx-5'
+        } text-lg rounded bg-gmdeepred p-1 md:p-2 font-bold transition-all text-gmgold hover:text-gmneonyellow hover:text-xl ${
+          isVideoFeedOnline ? 'animate-pulse' : ''
+        }`}
         onClick={() => {
           navigate(`/${name}`);
         }}
       >
-        {name.charAt(0).toUpperCase() +
-          name.slice(1)}
+        Join {name.charAt(0).toUpperCase() + name.slice(1)}
       </button>
     </li>
   );
