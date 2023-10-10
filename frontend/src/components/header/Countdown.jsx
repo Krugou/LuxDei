493ae84de04b90960ee9adec4b30c2f63c1a25c3
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-10-10 03:52:09 +0300

    countdown tyylittelyt

--- frontend/src/components/header/Countdown.jsx
+++ frontend/src/components/header/Countdown.jsx
@@ -1,40 +1,53 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-const Countdown = ({unixTime}) => {
-    const [timeLeft, setTimeLeft] = useState(0);
+import { useNavigate } from 'react-router-dom';
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            const now = Math.floor(Date.now() / 1000);
-            const secondsLeft = unixTime - now;
-            setTimeLeft(secondsLeft);
-        }, 1000);
-        return () => clearInterval(intervalId);
-    }, [unixTime]);
+const Countdown = ({ unixTime }) => {
+  const navigate = useNavigate();
 
-    const formatTime = (time) => {
-        const days = Math.floor(time / 86400);
-        const hours = Math.floor((time % 86400) / 3600);
-        const minutes = Math.floor((time % 3600) / 60);
-        const seconds = time % 60;
-        return `${days.toString().padStart(2, '0')}:${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    };
+  const [timeLeft, setTimeLeft] = useState(0);
 
-    return (
-        <div className="bg-gray-100 rounded-md p-2">
-            {timeLeft > 0 ? (
-                <p className="text-2xl text-center text-black font-bold">
-                    Time left until showtime: {formatTime(timeLeft)}
-                </p>
-            ) : (
-                ''
-            )}
-        </div>
-    );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = Math.floor(Date.now() / 1000);
+      const secondsLeft = unixTime - now;
+      setTimeLeft(secondsLeft);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [unixTime]);
+
+  const formatTime = (time) => {
+    const days = Math.floor(time / 86400);
+    const hours = Math.floor((time % 86400) / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${days.toString().padStart(2, '0')}:${hours
+      .toString()
+      .padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
+
+  return (
+    <div
+      className='bg-gmgold rounded-md p-2 cursor-pointer hover:bg-gmneonyellow'
+      onClick={() => {
+        navigate('/livestream');
+      }}
+    >
+      {timeLeft > 0 ? (
+        <p className='text-2xl text-center text-gmdeepblack font-bold '>
+          Time left until showtime: {formatTime(timeLeft)} - Click to tune in
+          live:
+        </p>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
 Countdown.PropTypes = {
-    unixTime: PropTypes.number.isRequired,
+  unixTime: PropTypes.number.isRequired,
 };
-
 
 export default Countdown;
